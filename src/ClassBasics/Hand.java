c221e1f752673771c4a53e5a1fe3c0c6d1cefc3b
Eric Chalmers <echalmers@mtroyal.ca>
2023-01-23 12:40:16 -0700

    add hand class and loop examples

--- src/ClassBasics/Hand.java
+++ src/ClassBasics/Hand.java
@@ -1,5 +1,34 @@
 package ClassBasics;
 
+import java.util.ArrayList;
+
 public class Hand {
+	
+	private ArrayList<Card> cards = new ArrayList<Card>();
+	
+	public void addCard(Card theCard) {
+		this.cards.add(theCard);
+	}
+	
+	public int total() {
+		
+		int totalScore = 0;
+		
+		for (int index = 0; index < this.cards.size(); index++ ) {
+			totalScore += this.cards.get(index).getValue();
+		}
+		
+		return totalScore;
+	}
+	
+	public String toString() {
+		String str = "";
+		
+		for (int index = 0; index < this.cards.size(); index++ ) {
+			str += this.cards.get(index).toString();
+		}
+		
+		return str;
+	}
 
 }
